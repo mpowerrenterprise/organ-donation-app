ffb1308224e11916973da48392988119c1c672d7
Gunarakulan Gunaretnam <gunarakulan@gmail.com>
2024-10-31 17:52:36 +0530

    docs: add app manual

--- source/mobile-app/lib/dashboard/message_sent_screen.dart
+++ source/mobile-app/lib/dashboard/message_sent_screen.dart
@@ -7,7 +7,6 @@ class MessageSentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
